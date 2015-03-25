08e545b30130a82eef14beb5805b22ba38c604c4
Senthur <senthurpandian.r@gmail.com>
2015-03-25 02:02:56 -0400

    Added SQLite

--- platforms/android/assets/www/js/index.js
+++ platforms/android/assets/www/js/index.js
@@ -17,35 +17,23 @@
  * under the License.
  */
 var app = {
-    // Application Constructor
-    initialize: function() {
-        this.bindEvents();
-    },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicitly call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-        app.receivedEvent('deviceready');
-    },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
+	initialize : function() {
+		this.bindEvents();
+	},
+	bindEvents : function() {
+		document.addEventListener('deviceready', this.onDeviceReady, false);
+	},
+	onDeviceReady : function() {
+		$(".status").text("Device is Ready");
+		console.log('Received Event: ' + id);
+		db.initialize();
+	},
+};
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-
-        console.log('Received Event: ' + id);
-    }
+var db = {
+	initialize : function() {
+		
+	}
 };
 
 app.initialize();
